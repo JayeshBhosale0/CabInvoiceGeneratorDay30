43648688517f4601b627a18fe2fec2e4223afff4
Jayesh <jayesh.bhosale1812@gmail.com>
2021-11-18 19:28:33 +0530

    Normal and premium rides

---
+++ InvoiceSummary.java
@@ -0,0 +1,34 @@
+package bridgelabz.cabinvoice;
+import java.util.Objects;
+public class InvoiceSummary {
+	int numOfRides;
+    double totalFare;
+    double avgFare;
+
+    public InvoiceSummary(int numberOfRides, double totalFare) {
+        this.numOfRides = numberOfRides;
+        this.totalFare = totalFare;
+    }
+    public InvoiceSummary(int numberOfRides, double totalFare, String type) {
+        this.numOfRides = numberOfRides;
+        this.totalFare = totalFare;
+    }
+
+    public InvoiceSummary getInvoiceSummary() {
+        this.avgFare = this.totalFare / this.numOfRides;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceSummary that = (InvoiceSummary) o;
+        return numOfRides == that.numOfRides && Double.compare(that.totalFare, totalFare) == 0 && Double.compare(that.avgFare, avgFare) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numOfRides, totalFare, avgFare);
+    }
+}
